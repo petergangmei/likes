07fb3315b646bc26fabf959a2bb60f1a37257c0e
petergangmei <topeter44msg@gmail.com>
2018-05-14 04:30:34 +0530

    edited notification list

--- resources/views/notification/notificationlist.blade.php
+++ resources/views/notification/notificationlist.blade.php
@@ -2,11 +2,9 @@
 @section('content')
 
 <ul class="list-group">
-  <li class="list-group-item">Cras justo odio</li>
-  <li class="list-group-item">Dapibus ac facilisis in</li>
-  <li class="list-group-item">Morbi leo risus</li>
-  <li class="list-group-item">Porta ac consectetur ac</li>
-  <li class="list-group-item">Vestibulum at eros</li>
+  <li class="list-group-item">Peter Accepted your request</li>
+  <li class="list-group-item">Vivek likek your post.</li>
+  <li class="list-group-item">Rabica commented in your post.</li>
 </ul>
 
 @endsection
