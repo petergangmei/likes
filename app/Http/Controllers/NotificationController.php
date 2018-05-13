fa2dc91534eb2f336e7c5a6c5e5a5d9c810aa98f
petergangmei <topeter44msg@gmail.com>
2018-05-14 05:09:14 +0530

    laravel notify removed, custom notificaation added

--- app/Http/Controllers/NotificationController.php
+++ app/Http/Controllers/NotificationController.php
@@ -6,20 +6,29 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Notifications\RequestAccepted;
 use App\User;
-
+use DB;
 
 class NotificationController extends Controller
 {
 
 	Public function view(){
-		return view('notification/notificationlist');
+		$user = DB::table('notification')->get();
+
+		return view('notification/notificationlist')->with('user', $user);
 	}
 
     	// auth()->user()->notify(new RequestAccepted());
     Public function notify(){
 
-    	auth()->user()->notify(new RequestAccepted());
-
+    	DB::table('customnotification')
+    	->insert([
+    		'user_id' => auth()->user()->id,
+    		'visitor_id' => '1',
+    		'data' => 'Peter accepted your request',
+    		'read' => 'unread',
+    		'type'=> 'request',
+    		'at' => '1233434'
+    	]);
     	return view('notification');
     }
 }
--- resources/views/notification/notificationlist.blade.php
+++ resources/views/notification/notificationlist.blade.php
@@ -1,6 +1,9 @@
 @extends('layouts.app2')
 @section('content')
 
+
+
+
 <ul class="list-group">
   <li class="list-group-item">Peter Accepted your request</li>
   <li class="list-group-item">Vivek likek your post.</li>
